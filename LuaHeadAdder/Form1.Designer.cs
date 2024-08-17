cbc8141f54577185f37d829f9295b4d5f8ea04c8
StackExplode <785532231@qq.com>
2024-08-17 10:52:52 +0800

    First Push

---
+++ LuaHeadAdder/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace LuaHeadAdder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "&Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 53);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(395, 332);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "--请先点击“Sync Code”下载待注入代码";
+            this.textBox2.WordWrap = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 391);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "&Inject";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 391);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "&Sync Code";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(309, 391);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "&Remove";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 36);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(41, 12);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Github";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(341, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Tips: There is no need to add comments as symbol anymore";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 423);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "LuaHeadAdder 1.2 - by Jennings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LuaHeadAdder/Form1.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LuaHeadAdder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var directoryDialog = new CommonOpenFileDialog
+            {
+                IsFolderPicker = true,
+                Title = "Select Folder",
+                DefaultDirectory = @"H:\World of Warcraft\_retail_\Interface\AddOns"
+            };
+            var rst = directoryDialog.ShowDialog();
+            if(rst == CommonFileDialogResult.Ok)
+            {
+                textBox1.Text = directoryDialog.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button4_Click(null,null);
+
+            //Scan all file by an extension filter including sub folder into an array
+            string[] files = System.IO.Directory.GetFiles(textBox1.Text, "*.lua", System.IO.SearchOption.AllDirectories);
+
+            string code = "--Begin Jennings Add \r\n";
+            code += textBox2.Text + "\r\n";
+            code += "--End Jennings Add \r\n";
+
+            foreach (var file in files)
+            {
+                //Append string to file's head and not overwrite the original file
+                System.IO.File.WriteAllText(file, code + System.IO.File.ReadAllText(file),Encoding.UTF8);
+
+            }
+            MessageBox.Show("Add all patch to lua files success!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string[] files = System.IO.Directory.GetFiles(textBox1.Text, "*.lua", System.IO.SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                StreamReader sr = new StreamReader(file,Encoding.UTF8);
+                MemoryStream ms = new MemoryStream();
+                StreamWriter sw = new StreamWriter(ms, Encoding.UTF8);
+                bool canread = true;
+                while(!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    if(line.Contains("--Begin Jennings Add"))
+                    {
+                        canread = false;
+                        continue;
+                    }
+                    else if(line.Contains("--End Jennings Add"))
+                    {
+                        canread = true;
+                        continue;
+                    }
+                    if(canread)
+                        sw.WriteLine(line);
+                }
+                sw.Close();
+                ms.Close();
+                sr.Close();
+                System.IO.File.WriteAllBytes(file, ms.ToArray());
+            }
+            if(sender != null)
+                MessageBox.Show("Remove all patch from lua files success!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Text = "Wait...";
+            string url = "https://ghproxy.net/https://raw.githubusercontent.com/StackExplode/WowLuaAPIAdaptiveHead/master/template.lua";
+            //Download string from url
+            string code = new System.Net.WebClient().DownloadString(url);
+            //Change code newline to windows style
+            code = code.Replace("\n", "\r\n");
+            textBox2.Text = code;
+            button3.Text = "&Sync Code";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/StackExplode/WowLuaAPIAdaptiveHead");
+        }
+    }
+}
